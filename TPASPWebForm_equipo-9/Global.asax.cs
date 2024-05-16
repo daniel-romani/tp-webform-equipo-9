7b7a0f05d43deba9601d26b2d5ac3de5584798ef
Legore <gastonromani@gmail.com>
2024-05-16 02:00:34 -0300

    Template borrado

--- TPASPWebForm_equipo-9/Global.asax.cs
+++ TPASPWebForm_equipo-9/Global.asax.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Web;
-using System.Web.Optimization;
-using System.Web.Routing;
-using System.Web.Security;
-using System.Web.SessionState;
 
 namespace TPASPWebForm_equipo_9
 {
@@ -13,9 +7,6 @@ namespace TPASPWebForm_equipo_9
     {
         void Application_Start(object sender, EventArgs e)
         {
-            // Code that runs on application startup
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
